1609ace0b9b852d13b1fc944dc3a0e805e51b4e3
shaamkp <shyamk.accolades@gmail.com>
2023-08-11 16:09:07 +0530

    Changed | Day list api changed from pk to query param

--- api/v1/courses/urls.py
+++ api/v1/courses/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     re_path(r'^edit-daily-topics/(?P<pk>.*)/$',views.edit_daily_topics, name="edit-daily-topics"), 
     re_path(r'^delete-daily-topics/(?P<pk>.*)/$',views.delete_daily_topics, name="delete-daily-topics"), 
 
-    re_path(r'^days-list/(?P<pk>.*)/$',views.days_list_programme, name="list-of-days-in-programme"),
+    re_path(r'^days-list/$',views.days_list_programme, name="list-of-days-in-programme"),
     re_path(r'^add-days/(?P<pk>.*)/$',views.add_days, name="add-days"),
     re_path(r'^add-day/(?P<pk>.*)/$',views.add_day, name="add-day"),
     re_path(r'^student-count/$',views.student_count, name="student-count"),
