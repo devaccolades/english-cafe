91d2406e2beeaa4f63e2dc52fca650f2f650ed81
shaamkp <shyamk.accolades@gmail.com>
2023-08-21 15:30:41 +0530

    Added | single number of content

--- api/v1/courses/urls.py
+++ api/v1/courses/urls.py
@@ -30,6 +30,7 @@ urlpatterns = [
     re_path(r'^add-days/(?P<pk>.*)/$',views.add_days, name="add-days"),
     re_path(r'^add-day/(?P<pk>.*)/$',views.add_day, name="add-day"),
     re_path(r'^add-number-of-content-in-a-day/(?P<pk>.*)/$',views.add_number_of_content, name="add-day"),
+    re_path(r'^number-of-content-in-a-day/(?P<pk>.*)/$',views.number_of_content, name="number-of-content-in-a-day"),
     re_path(r'^student-count/$',views.student_count, name="student-count"),
 
 
