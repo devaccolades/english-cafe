b1c15b83ff37e7372b3943d31a99b0c42d33b7f3
shaamkp98 <shyam.woodenclods@gmail.com>
2023-10-25 17:17:34 +0530

    Added | tags to the api

--- api/v1/general/urls.py
+++ api/v1/general/urls.py
@@ -5,6 +5,8 @@ app_name = "api_v1_general"
 
 urlpatterns = [
     re_path(r'^list-blogs/$', views.list_blogs, name="programmes"),
+    re_path(r'^list-tags/$', views.list_tags, name="tags"),
+    re_path(r'^list-blogs-tags/(?P<pk>.*)/$', views.list_blogs_tags, name="blog-tags"),
     path('single-blogs/<slug:slug>/', views.single_blog, name="single-blog"),
 
 ]
--- api/v1/general/views.py
+++ api/v1/general/views.py
@@ -8,8 +8,8 @@ from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 
-from general.models import Blog
-from api.v1.general.serializers import ListBlogSerializer
+from general.models import Blog, Tags
+from api.v1.general.serializers import ListBlogSerializer, ListTagsSerializer
 
 
 @api_view(['GET'])
@@ -17,7 +17,7 @@ from api.v1.general.serializers import ListBlogSerializer
 def list_blogs(request):
     try:
         if (blogs := Blog.objects.filter(is_deleted=False)).exists():
-            blogs = blogs.order_by("date_added")
+            blogs = blogs.order_by("-date_added")
 
             serialized_data = ListBlogSerializer(
                 blogs,
@@ -95,3 +95,100 @@ def single_blog(request, slug):
         }
 
     return Response({'app_data': response_data}, status=status.HTTP_200_OK)
+
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def list_tags(request):
+    try:
+        if (tags := Tags.objects.filter(is_deleted=False)).exists():
+            tags = tags.order_by("-date_added")
+
+            serialized_data = ListTagsSerializer(
+                tags,
+                context = {
+                    "request" : request
+                },
+                many = True
+            ).data
+
+            response_data = {
+                "StatusCode": 6000,
+                "data" : serialized_data
+            }
+        else:
+            response_data = {
+                "StatusCode" : 6001,
+                "data" : []
+            }
+
+        transaction.set_autocommit(False)
+    except Exception as e:
+        transaction.rollback()
+        errType = e.__class__.__name__
+        errors = {
+            errType: traceback.format_exc()
+        }
+        response_data = {
+            "status": 0,
+            "api": request.get_full_path(),
+            "request": request.data,
+            "message": str(e),
+            "response": errors
+        }
+
+    return Response({'app_data': response_data}, status=status.HTTP_200_OK)
+
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def list_blogs_tags(request, pk):
+    try:
+        if (tags := Tags.objects.filter(pk=pk, is_deleted=False)).exists():
+            tags = tags.latest("date_added")
+            
+            if (blogs := Blog.objects.filter(tags=tags, is_deleted=False)).exists():
+                blogs = blogs.order_by("-date_added")
+
+                serialized_data = ListBlogSerializer(
+                    blogs,
+                    context = {
+                        "request" : request
+                    },
+                    many = True
+                ).data
+
+                response_data = {
+                    "StatusCode": 6000,
+                    "data" : serialized_data
+                }
+            else:
+                response_data = {
+                    "StatusCode" : 6001,
+                    "data" : []
+                }
+        else:
+            response_data = {
+                "StatusCode" : 6001,
+                "data" : {
+                    "title" : "Failed",
+                    "message" : "Tags not found"
+                }
+            }
+
+    except Exception as e:
+        transaction.rollback()
+        errType = e.__class__.__name__
+        errors = {
+            errType: traceback.format_exc()
+        }
+        response_data = {
+            "status": 0,
+            "api": request.get_full_path(),
+            "request": request.data,
+            "message": str(e),
+            "response": errors
+        }
+
+    return Response({'app_data': response_data}, status=status.HTTP_200_OK)
+
